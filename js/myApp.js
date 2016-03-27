ba00ecf9e650640b731ceb9c356130f49ffa1c71
Lynsey Teague <lynsey.teague@yahoo.com>
2016-03-27 15:38:54 -0400

    working on js2

--- js/myApp.js
+++ js/myApp.js
@@ -1,65 +1,109 @@
-angular.module('myApp', [])
-    .controller('calculations', ['$scope', function ($scope) {
-        $scope.mealCount = 1;
+var app = angular.module('myApp', [])
 
-        $scope.submit = function () {
-            if ($scope.myForm.$valid) {
-                $scope.mealCount++;
+app.service('earnings', function () {
+    var meals = [];
 
-                var price = parseFloat($scope.price);
-                var tax = parseFloat($scope.tax);
-                var tip = parseFloat($scope.tip);
+    var earningsData = {
+        mealCount: 0,
+        tipTotal: 0,
+        tipAverage: 0
+    };
 
-                //customer-charges
-                $scope.customerSubtotal = price + (price * (tax / 100));
-                $scope.customerTip = $scope.customerSubtotal * (tip / 100);
-                $scope.customerTotal = $scope.customerSubtotal + $scope.customerTip;
+    return {
+        addMeal: function (meal) {
+            meals.push(meal);
+            earningsData.mealCount++;
+            earningsData.tipTotal += meal.tip;
+            earningsData.tipAverage = (earningsData.tipTotal / earningsData.mealCount);
+            console.log(earningsData)
+        },
+        getMeals: function () {
+            return meals;
+        },
+        getEarningsData: function () {
+            return earningsData;
+        },
+        clearAll: function () {
+            meals.length = 0;
+            earningsData = {
+                mealCount: 0,
+                tipTotal: 0,
+                tipAverage: 0
+            };
+        }
+    };
+});
 
-                //clear user input from form
-                $scope.cancelMeal();
-            }
+app.controller('detailsController', function ($scope, earnings) {
+    $scope.mealCount = 1;
+
+    //clear meal details
+    $scope.cancelMeal = function () {
+        $scope.price = "";
+        $scope.tax = "";
+        $scope.tip = "";
+    };
+
+    $scope.submit = function () {
+        if ($scope.myForm.$valid) {
+            $scope.mealCount++;
+
+            var price = parseFloat($scope.price);
+            var tax = parseFloat($scope.tax);
+            var tip = parseFloat($scope.tip);
+
+            //customer-charges
+            $scope.customerSubtotal = price + (price * (tax / 100));
+            $scope.customerTip = $scope.customerSubtotal * (tip / 100);
+            $scope.customerTotal = $scope.customerSubtotal + $scope.customerTip;
+
+            var meal = {
+                subtotal: $scope.customerSubtotal,
+                tip: $scope.customerTip,
+                total: $scope.customerTotal
+            };
+            earnings.addMeal(meal);
+
+            //clear user input from form
+            $scope.cancelMeal();
         };
+    };
+});
 
-        //clear meal details
-        $scope.cancelMeal = function () {
-            $scope.price = "";
-            $scope.tax = "";
-            $scope.tip = "";
-        };
-    }])
 
-.controller('earnings', ['$scope', function ($scope) {
-        var meals = [];
+app.controller('chargesController', function ($scope, earnings) {
+    $scope.getMeals = function () {
+        var meals = earnings.getMeals();
+        $scope.meals = meals;
+        console.log($scope.meals);
+    };
 
-        var earningsData = {
-            mealCount: 0,
-            tipTotal: 0,
-            tipAverage: 0
-        };
+    $scope.getMeals();
 
-        return {
-            addMeal: function (meal) {
-                meals.push(meal);
-                earningsData.mealCount++;
-                earningsData.tipTotal += meal.customerTip;
-                earningsData.tipAverage = (earningsData.tipTotal / earningsData.mealCount);
-            },
-            getMeals: function () {
-                return meals;
-            },
-            getEarningsData: function () {
-                return earningsData;
-            },
-            clearAll: function () {
-                meals.length = 0;
-                earningsData = {
-                    mealCount: 0,
-                    tipTotal: 0,
-                    tipAverage: 0
-                };
-            }
-        };
-    }])
-    //define meal count
-    //define tip total
-    //define tip average
+    $scope.mealCount = $scope.meals.length;
+
+    $scope.avoidZero = function () {
+        if ($scope.mealCount === 0) {
+            $scope.mealCount = 1;
+        }
+    };
+
+    $scope.avoidZero();
+
+});
+
+
+app.controller('earningsController', function ($scope, earnings) {
+    $scope.mealCount = earningsData.mealCount;
+    $scope.tipTotal = earningsData.getEarningsData().tipTotal;
+    $scope.tipAverage = earningsData.getEarningsData().tipAverage;
+
+});
+
+
+//rest entire calculator
+app.controller('resetController', function ($scope, earnings) {
+    $scope.clearAll = function () {
+        earnings.clearAll();
+    };
+});
